1c991a0dd4eccab6994cfda254a0469c51af8ce5
Del Hatch <engineering_type@yahoo.com>
2024-01-27 11:39:39 -0800

    Changed from try-catch to "if key in dict"

--- Sofware/playback.py
+++ Sofware/playback.py
@@ -5,10 +5,17 @@ import os
 import csv
 import taglib
 
+
 class music():
     volume = 60
     playlist = [["", "", "", ""]]
     currentSongIndex = 0
+    AlbumNoKey = 0
+    AlbumEmptyField = 0
+    ArtistNoKey = 0
+    ArtistEmptyField = 0
+    TitleNoKey = 0
+    TitleEmptyField = 0
 
     def __init__(self):
         self.vlcInstance = vlc.Instance()
@@ -126,30 +133,40 @@ class music():
                 print("Error parsing tags")
 
             # Check to see if the "ARTIST" field is empty, or does not exist.
-            #    If so, copy in the info from the "ALBUMARTIST" field.
-            try:
-                if( song["ARTIST"] == [] ):
-                    # If here, there WAS an ARTIST entry, and it was empty.
-                    song["ARTIST"] = ['NOT SURE ARTIST']  # Found none of these
-            except:
-                # If here, there was no entry for ARTIST. void/null field.
-                song["ARTIST"] = ['Not Sure ARTIST']  # Found 26 of these
+            #    If so, fill it in with "Not Sure"
+            if( 'ARTIST' in song ):
+                if( song['ARTIST'] == [] ):  # Key exists, but points to empty field.
+                    self.ArtistEmptyField += 1
+                    song['ARTIST'] = ['NOT SURE ARTIST']  # Found none of these
+                else:
+                    pass  # Artist field was filled in. Is legit.
+            else:
+                self.ArtistNoKey += 1
+                song['ARTIST'] = ['Not Sure ARTIST']   # Found 26 of these
 
             # Check to see if the "ALBUM" field is empty, or does not exist.
             #    If so, fill it in with "Not Sure"
-            try:
-                if( song["ALBUM"] == [] ):
-                    tags["ALBUM"] = ['NOT SURE ALBUM']  # Found 1635 of these
-            except:
-                song["ALBUM"] = ['Not Sure ALBUM']   # Found 174 of these
+            if( 'ALBUM' in song ):
+                if( song['ALBUM'] == [] ):  # Key exists, but points to empty field.
+                    self.AlbumEmptyField += 1
+                    song['ALBUM'] = ['NOT SURE ALBUM']  # Found 1635 of these
+                else:
+                    pass  # Album field was filled in. Is legit.
+            else:
+                self.AlbumNoKey += 1
+                song['ALBUM'] = ['Not Sure ALBUM']   # Found 174 of these
 
             # Check to see if the "TITLE" field is empty, or does not exist.
             #    If so, fill it in with "Not Sure"
-            try:
-                if( song["TITLE"] == [] ):
-                    song["TITLE"] = ['NOT SURE TITLE']  # None of these
-            except:
-                song["TITLE"] = ['Not Sure TITLE'] # Found 41 of these
+            if( 'TITLE' in song ):
+                if( song['TITLE'] == [] ):  # Key exists, but points to empty field.
+                    self.TitleEmptyField += 1
+                    song['TITLE'] = ['NOT SURE TITLE']  # Found none of these
+                else:
+                    pass  # Album field was filled in. Is legit.
+            else:
+                self.TitleNoKey += 1
+                song['TITLE'] = ['Not Sure TITLE']   # Found 41 of these
 
             # At this point, the following writer call should never fail.
             try:
@@ -171,5 +188,11 @@ class music():
                 except:
                     pass
         file.close()
-        print("Done writing metadata file."
+        print("Done writing metadata file.")
+        print("AlbumNoKey = ", self.AlbumNoKey)
+        print("AlbumEmptyField = ", self.AlbumEmptyField)
+        print("ArtistNoKey = ", self.ArtistNoKey)
+        print("ArtistEmptyField = ", self.ArtistEmptyField)
+        print("TitleNoKey = ", self.TitleNoKey)
+        print("TitleEmptyField = ", self.TitleEmptyField)
         return 1
